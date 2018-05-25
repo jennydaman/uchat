bcfa60f868d3973f94aa30321a1d6e9261a41614
Jennings Zhang <jenni_zh@protonmail.com>
2018-05-25 14:37:41 -0400

    Fix layout issues and add MessageGroup border radii

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@
 
       <v-list dense>
         <v-divider></v-divider>
-        <v-list-tile v-for="friend in friends" :key="friend.name" @click="">
+        <v-list-tile v-for="friend in conversations" :key="friend.name" @click="">
           <v-list-tile-avatar>
             <img v-bind:src="friend.pic" >
           </v-list-tile-avatar>
@@ -40,90 +40,38 @@
 
     </v-navigation-drawer>
 
-
-    <v-content>
-      <div id="content">
-      <v-container fluid>
-        <message-group v-for="group in data" v-bind:data="group"></message-group>
-      </v-container>
-      </div>
-    </v-content>
-
-    <v-footer app>
+    <v-footer app fixed class="hidden-md-and-down">
       <span>&copy; uchat 2018. Made at <a href="https://hacks.mbhs.edu/" target="_blank">BlairHacks_0</a>.</span>
     </v-footer>
-    <div id="chatpos">
+
+    <div id="chatpos" v-bind:class="{ftspace: $vuetify.breakpoint.lgAndUp}">
       <div style="padding-left: 300px" class="hidden-md-and-down"></div>
     <chat-box></chat-box>
     </div>
 
+    <v-content>
+      <v-container fluid id="messageArea">
+        <message-group v-for="group in messageStream" v-bind="group"></message-group>
+        <div style="height: 56px"></div> <!-- space for the chat box -->
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
 import MessageGroup from './components/MessageGroup.vue'
 import ChatBox from './components/ChatBox.vue'
+
+import * as Model from './model'
 export default {
   name: 'app',
   data: () => {
     return {
       drawer: true,
       person: 'person',
-      user: {
-        name: 'Jennings Zhang',
-        pic: 'https://jennydaman.github.io/assets/square_swan.jpg'
-      },
-      friends: [
-        {
-          name: 'Noah Singer',
-          pic: 'https://angstromctf.com/assets/images/people/Noah%20Singer.jpg'
-        },
-        {
-          name: 'Noah Kim',
-          pic: 'https://angstromctf.com/assets/images/people/Noah%20Kim.jpg'
-        },
-        {
-          name: 'Ian Rackow',
-          pic: 'https://angstromctf.com/assets/images/people/Ian%20Rackow.jpg'
-        }
-      ],
-      data: [
-        {
-          platform: 'discord',
-          isAuthor: false,
-          author: 'Noah Singer',
-          messages: ['hey', 'how are you?']
-        },
-        {
-          platform: 'discord',
-          isAuthor: true,
-          author: 'Jennings Zhang',
-          messages: [
-            'they ask you how you are',
-            "and you have to say you're fine",
-            "but you're not really fine"
-          ]},
-        {
-          platform: 'hangouts',
-          isAuthor: false,
-          author: 'Noah Singer',
-          messages: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sem arcu, pulvinar eu mauris eget, vestibulum aliquam nisi. Etiam blandit leo lorem, quis gravida nunc faucibus non. Suspendisse potenti. Nam auctor mollis mi sit amet eleifend. Donec nec ornare sem. Integer diam sem, auctor ac ex eget, molestie maximus quam. Fusce pellentesque nisl ac erat vestibulum hendrerit. Praesent ultricies malesuada rhoncus.',
-            "Morbi facilisis, augue sit amet imperdiet varius, dui ipsum vestibulum orci, in tempus enim massa nec neque. Fusce facilisis a mi vitae ornare. Phasellus in enim lorem. Praesent malesuada varius lectus vel eleifend. Sed tempus metus et est tempus, nec eleifend sem posuere. Donec laoreet vitae purus quis varius. Aliquam semper lacus sed laoreet consectetur.",
-            "Sed eget malesuada ligula. Vivamus a tempor magna. Ut ultricies vel lectus eu viverra. Donec at ligula nunc. Nam tempor a magna at auctor. Morbi nec pulvinar elit. Donec tincidunt elit massa, tristique facilisis odio ultrices quis. Ut accumsan a urna in luctus. Donec porttitor sed odio ac molestie.",
-            "In hac habitasse platea dictumst. Integer massa quam, scelerisque vel tellus eu, hendrerit porta turpis. Aliquam erat volutpat. Vestibulum varius lacus sit amet porttitor vulputate. Quisque et justo sed leo pharetra egestas. Integer finibus consectetur metus ac auctor. Aenean purus dui, finibus vel metus eget, convallis tempor ligula. Suspendisse ac accumsan dui. Sed sagittis, tortor sit amet sollicitudin sollicitudin, leo nibh venenatis ipsum, non condimentum turpis velit maximus magna. Nullam non libero eu elit bibendum ullamcorper. Duis a dui sit amet sapien bibendum rhoncus. Aenean sagittis dignissim ipsum, ut finibus ex vestibulum eget. Suspendisse eu nibh pulvinar, pellentesque ipsum quis, vehicula nibh. Nulla aliquet sagittis semper."
-          ]},
-        {
-          platform: 'hangouts',
-          isAuthor: false,
-          author: 'Noah Singer',
-          messages: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sem arcu, pulvinar eu mauris eget, vestibulum aliquam nisi. Etiam blandit leo lorem, quis gravida nunc faucibus non. Suspendisse potenti. Nam auctor mollis mi sit amet eleifend. Donec nec ornare sem. Integer diam sem, auctor ac ex eget, molestie maximus quam. Fusce pellentesque nisl ac erat vestibulum hendrerit. Praesent ultricies malesuada rhoncus.',
-            "Morbi facilisis, augue sit amet imperdiet varius, dui ipsum vestibulum orci, in tempus enim massa nec neque. Fusce facilisis a mi vitae ornare. Phasellus in enim lorem. Praesent malesuada varius lectus vel eleifend. Sed tempus metus et est tempus, nec eleifend sem posuere. Donec laoreet vitae purus quis varius. Aliquam semper lacus sed laoreet consectetur.",
-            "Sed eget malesuada ligula. Vivamus a tempor magna. Ut ultricies vel lectus eu viverra. Donec at ligula nunc. Nam tempor a magna at auctor. Morbi nec pulvinar elit. Donec tincidunt elit massa, tristique facilisis odio ultrices quis. Ut accumsan a urna in luctus. Donec porttitor sed odio ac molestie.",
-            "In hac habitasse platea dictumst. Integer massa quam, scelerisque vel tellus eu, hendrerit porta turpis. Aliquam erat volutpat. Vestibulum varius lacus sit amet porttitor vulputate. Quisque et justo sed leo pharetra egestas. Integer finibus consectetur metus ac auctor. Aenean purus dui, finibus vel metus eget, convallis tempor ligula. Suspendisse ac accumsan dui. Sed sagittis, tortor sit amet sollicitudin sollicitudin, leo nibh venenatis ipsum, non condimentum turpis velit maximus magna. Nullam non libero eu elit bibendum ullamcorper. Duis a dui sit amet sapien bibendum rhoncus. Aenean sagittis dignissim ipsum, ut finibus ex vestibulum eget. Suspendisse eu nibh pulvinar, pellentesque ipsum quis, vehicula nibh. Nulla aliquet sagittis semper."
-          ]}
-      ]
+      user: Model.getUser(),
+      conversations: Model.getConversations(),
+      messageStream: Model.getMessageStream()
     }
   },
   props: {
@@ -135,10 +83,8 @@ export default {
 }
 </script>
 
-<style>
-html {
-  overflow-y: hidden;
-}
+<style lang="scss">
+
 #app {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
@@ -146,21 +92,20 @@ html {
   -moz-osx-font-smoothing: grayscale;
 }
 
-#logo {
-  width: 300px;
-}
-
-#content {
-  overflow-y: scroll;
-  height: calc(100% - 300px);
+#messageArea {
+  height: 100%;
 }
 
 #chatpos {
   position: fixed;
   display: flex;
   width: 100%;
-  bottom: 12px;
+  bottom: 0;
+  z-index: 1;
 }
 
+#chatpos.ftspace {
+  bottom: 12px;
+}
 
 </style>
--- src/components/MessageGroup.vue
+++ src/components/MessageGroup.vue
@@ -1,27 +1,37 @@
 <!-- groups together messages sent by the same person within a short amount of time. -->
 
 <template>
-  <v-layout
+  <p><v-layout
   column
   fill-height
-  v-bind:align-start="!data.isAuthor"
-  v-bind:align-end="data.isAuthor"
+  v-bind:align-start="!isAuthor"
+  v-bind:align-end="isAuthor"
+  class="mgroup"
+  v-bind:class="isAuthor ? 'mright' : 'mleft'"
   >
-    <v-flex v-for="message in data.messages">
-      <v-chip
-        v-bind:color="data.isAuthor ? 'white' : $bg_color[data.platform]"
-        v-bind:text-color="data.isAuthor ? 'black' : 'white'"
-        class="message">{{ message }}</v-chip>
-    </v-flex>
-    
-  </v-layout>
+    <span v-for="message in messages" class="message" v-bind:style="special">{{ message }}</span>
+  </v-layout></p>
 </template>
 
 <script>
 export default {
   name: 'message-group',
   props: {
-    data: Object
+    platform: String,
+    isAuthor: Boolean,
+    author: String,
+    messages: Array
+  },
+  computed: {
+    special: function() {
+      const bg = this.isAuthor ? "white" : this.$bg_color[this.platform];
+      const fg = this.isAuthor ? "black" : "white";
+
+      return {
+        'background-color': bg ,
+        color: fg
+      }
+    }
   }
 }
 </script>
@@ -29,14 +39,43 @@ export default {
 <style>
 
 .message {
-  margin-top: 1px;
-  margin-bottom: 0px;
   max-width: 600px;
+  border: 1px solid transparent;
+  font-size: 14px;
+  padding: 6px 12px;
+  margin-bottom: 1px;
 }
 
-.message .chip__content {
-  white-space: normal;
-  padding: 6px 12px;
-  height: initial;
+@media only screen and (max-width: 720px) {
+  .message {
+    max-width: 80%;
+  }
+}
+
+.mgroup {
+  --r: 16px;
+}
+
+.mgroup.mleft span:first-child {
+  border-top-left-radius: var(--r);
+}
+.mgroup.mleft span {
+  border-top-right-radius: var(--r);
+  border-bottom-right-radius: var(--r);
+}
+.mgroup.mleft span:last-child {
+  border-bottom-left-radius: var(--r);
+}
+
+
+.mgroup.mright span:first-child {
+  border-top-right-radius: var(--r);
+}
+.mgroup.mright span {
+  border-top-left-radius: var(--r);
+  border-bottom-left-radius: var(--r);
+}
+.mgroup.mright span:last-child {
+  border-bottom-right-radius: var(--r);
 }
 </style>
