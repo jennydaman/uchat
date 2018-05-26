55e7574c41fa2abc8a3a15faff981a11f59f2a4e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-26 20:42:52 +0000

    Deploy jennydaman/uchat to github.com/jennydaman/uchat.git:gh-pages

---
+++ precache-manifest.07a208b14e4ed7c5d6ea30f9c8d0a386.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a0e672afd81013a6ae37bd015a1447b",
+    "url": "/mock/stream/51c6c72a8666855a729eb35d7d686924.json"
+  },
+  {
+    "revision": "45208f641dca2175ccf9",
+    "url": "/js/vendors~app.45208f64.js"
+  },
+  {
+    "revision": "eab6f960fd97cce1dbc299b825d8b1a3",
+    "url": "/mock/user.json"
+  },
+  {
+    "revision": "3fb2155b5ba9e40b0f89c8e8216bca5b",
+    "url": "/mock/stream/README.md"
+  },
+  {
+    "revision": "40dcb4bc70302814e60c396bf6ae65ce",
+    "url": "/mock/stream/aff64ded417c621c501735157f4ce4ba.json"
+  },
+  {
+    "revision": "bca8276cadd84b18f0028100d27e4020",
+    "url": "/landing/style.css"
+  },
+  {
+    "revision": "8bc6537fbb6af572be9d16f2e477e4cd",
+    "url": "/landing/SWgoogle.png"
+  },
+  {
+    "revision": "fe6bd9bd0678dbc2d130454b6c3bab9e",
+    "url": "/mock/conversations.json"
+  },
+  {
+    "revision": "2b73799655022bb43cf4443cb0815f64",
+    "url": "/mock/stream/5344c381b1e84a579bd44bd237f3d06a.json"
+  },
+  {
+    "revision": "e9dd6d83a6a032cd0c91",
+    "url": "/js/app.e9dd6d83.js"
+  },
+  {
+    "revision": "7f3d861e5931b3d705580458a9a6f285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fc3351ab596a648372d2118ed88ee72",
+    "url": "/img/third_party/hangouts.png"
+  },
+  {
+    "revision": "e140eb5db5d3691d7074385786ee8502",
+    "url": "/img/third_party/hangouts.jpg"
+  },
+  {
+    "revision": "5aa7f3b38be0614ae9619035c83f2b2f",
+    "url": "/img/third_party/discord.svg"
+  },
+  {
+    "revision": "1bb365e858673969bcfe49341a6339e0",
+    "url": "/img/Full Logo.png"
+  },
+  {
+    "revision": "e9dd6d83a6a032cd0c91",
+    "url": "/css/app.ad5ba737.css"
+  }
+];
